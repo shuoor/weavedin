38799e5f7eb2aa6d7af1d6fbb027e5ddb74bd336
Unknown <mail.shuaibkhan@gmail.com>
2018-09-30 14:38:39 +0530

    First Commit

---
+++ js/comments.js
@@ -0,0 +1,305 @@
+/*
+ *  CommentsJS - v0.0.1
+ *
+ *  Made by Rnjai Lamba
+ *  Under MIT License
+ */
+// the semi-colon before function invocation is a safety net against concatenated
+// scripts and/or other plugins which may not be closed properly.
+;( function( $, window, document, undefined ) {
+
+		// Create the defaults once
+		var pluginName = "comments",
+			defaults = {
+				propertyName: "value"
+			};
+		var className = 'anchor';
+		var idcache = {};
+		var count = 0;
+
+		// The actual plugin constructor
+		function Plugin ( element, options ) {
+			this.element = element;
+			this.settings = $.extend( {}, defaults, options );
+			this._defaults = defaults;
+			this._name = pluginName;
+			this.init(options,element);
+		}
+
+		// Replace element with initial html
+		function addBasicHTML ( element ) {
+			element = $(element);
+			var className = element.attr('class');
+			var idName = element.attr('id');
+			if(typeof className != 'undefined')
+				$('.'+className).append('<div class = "container"> <div class="leave-a-reply"> <span class="title">Leave a Comment</span> <div class="row"> <div class="col-md-12"> <textarea name="comment" id="comment" class="form-control" rows="8" placeholder="Message"></textarea> </div> <div class="col-md-12"> <button type="submit" class="btn-black">Publish Comment</button> </div> </div> </div> <!-- End .leave-a-reply --> <div class="comment"> <span class="title blog-comments">All Comments ( Write something above and click publish to see it here )</span> </div> <!-- End .commnet --></div> <!-- End .container -->');
+			if(typeof idName != 'undefined')
+				$('#'+idName).append('<div class = "container"> <div class="leave-a-reply"> <span class="title">Leave a Comment</span> <div class="row"> <div class="col-md-12"> <textarea name="comment" id="comment" class="form-control" rows="8" placeholder="Message"></textarea> </div> <div class="col-md-12"> <button type="submit" class="btn-black">Publish Comment</button> </div> </div> </div> <!-- End .leave-a-reply --> <div class="comment"> <span class="title blog-comments">All Comments ( Write something above and click publish to see it here )</span> </div> <!-- End .commnet --></div> <!-- End .container -->');
+
+		}
+
+		// Literally get ready for comments
+		function getReadyForComments ( ) {
+
+			/* ======================================
+			     IS ELEMENT OF TYPE 
+			   ====================================== */
+
+			   function isElementType(element,type){
+			      return $(element).is(type);
+			   }
+
+
+			/* ======================================
+			     GET DATE FORMATTED
+			   ====================================== */
+
+			   function getDateFormatted(d){
+
+			    var month = d.getMonth()+1;
+			    var day = d.getDate();
+
+			    return output = d.getFullYear() + '/' +
+			        (month<10 ? '0' : '') + month + '/' +
+			        (day<10 ? '0' : '') + day; 
+			  }  
+
+
+			/* ======================================
+			     CREATE COMMENT REPLY
+			   ====================================== */
+			    function createCommentReply(){
+
+			      var outerMostDiv = $('<div>')
+			                              .attr("class", "leave-a-reply-to-comment");      
+			      var outerDiv = $('<div>')
+			                          .attr("class", "row");
+			      var form = $('<form>')
+			                        .attr({ action:"#", method:"POST" });
+			      var textArea = $('<textarea>')
+			                          .attr({ name:"comment", id:"comment" , class:"form-control" , rows:"8" , placeholder:"message" });
+			      var button = $('<button>')
+			                          .attr({ type:"submit", id:"class" , class:"btn-black" })
+			                          .text('Reply to Comment');
+			      var divButtonText =  $('<div>')
+			                                .attr("class", "col-md-12");
+
+			      form.append(divButtonText.append(textArea));                                             
+			      form.append(divButtonText.append(button));  
+			      return outerMostDiv.append(outerDiv.append(form));                                          
+
+			    }
+
+			    
+			/* ======================================
+			     CREATE COMMENT WITH MARGIN
+			   ====================================== */
+			    function createCommentWithMargin(data){
+			      var marginLeft = parseInt($( data.aboveElement ).css( "margin-left" ));   
+			      var isElementSpan = isElementType(data.aboveElement,"span"); //true or false
+			      if(isElementSpan)
+			        marginLeft = -60;
+			      var params = {};
+			      params.marginLeft = marginLeft;
+			      return createComment(data,params);
+
+			    }    
+
+
+			/* ======================================
+			     CREATE COMMENT 
+			   ====================================== */
+			    function createComment(data,params){
+
+			      var outerMostDiv = $('<div>')
+			                              .attr("class", "comment-wrap")   
+			                              .css("margin-left",params.marginLeft+60);                                             
+
+			      var commentDiv = $('<div>')
+			                              .attr("class", "full-comment");
+			      var commentHeading = $('<h5>');
+			      var commentHeadingLink = $('<a>')
+			                                  .attr("href", "#")
+			                                  .text("Your Comment");   
+			      var commentSpan = $('<a>')
+			                            .attr("class", "date")
+			                            .text(getDateFormatted(new Date()));     
+			      var commentParagraph = $('<p>')
+			                            .text(String(data.commentText));  
+			      var commentReplyDiv = $('<div>')
+			                                .attr("class", "reply");
+			      var commentReplyLink = $('<a>')
+			                                  .attr({ class:"btn-white-sm", href:"#" })
+			                                  .text("Reply");  
+			      var showMoreReplyDiv = $('<div>')
+			                                .attr("class", "show-reply");                                  
+			      var showMoreReplyLink = $('<a>')
+			                                  .attr({ class:"btn-white-sm", href:"#" })
+			                                  .text("Show replies");                                    
+
+			      commentHeadingLink.css("color","red"); // Just to show dynamically that it is new
+
+			      commentReplyDiv.append(commentReplyLink);    
+			      commentDiv.append(commentHeading.append(commentHeadingLink));    
+			      commentDiv.append(commentSpan);    
+			      commentDiv.append(commentParagraph);    
+			      commentDiv.append(commentReplyDiv);    
+			    
+			      outerMostDiv.append(commentDiv);
+
+			      return outerMostDiv;                                          
+
+			    }    
+
+			    //CHECK INPUT TEXT FIELD EMPTY
+			    // ==============================================
+			    function checkInputTextFieldEmpty(element){
+			      var myfield = $(element).val();
+			      if(myfield.length == 0){
+			        return true;  
+			      }
+			      else{
+			        return false; 
+			      }
+
+			    }
+
+
+			  /* ======================================
+			       REMOVE THE COMMENT BOX
+			     ====================================== */
+			      function removeCommentBox(){
+
+			        $( ".comment .leave-a-reply-to-comment" ).remove( );                                     
+
+			      }
+
+
+			  /* ======================================
+			       REMOVE TOP LEVEL PUBLISH COMMENT TEXT
+			     ====================================== */
+			      function removeTextTopLevel(){
+
+			        var x = $('.leave-a-reply').find('#comment').val('');                          
+
+			      }  
+
+
+			  /* ======================================
+			       CHECK BLOGID = PARENTID
+			     ====================================== */
+			      function checkBlogidParentid(data){
+
+			        if( data.blogId == data.parentId) return true;
+			        else return false                      
+
+			      }      
+
+
+			  /* ======================================
+			       ADD ELEMENT DYNAMICALLY - AFTER CLICK CALL
+			     ====================================== */
+			      function addCommentDynamically(data){
+			        var comment =  createCommentWithMargin(data);
+			        $( comment ).insertAfter(data.aboveElement);
+			        removeCommentBox();
+			        removeTextTopLevel();
+			        $('html, body').animate({
+			          scrollTop: (data.aboveElement).offset().top -85
+			        }, 500);
+			      }    
+			$( document ).ready(function() {
+
+
+			/* ======================================
+			     ON CLICKING TOP LEVEL PUBLISH COMMENT
+			   ====================================== */
+			   $(document).on('click','div.leave-a-reply .btn-black',function(){
+
+			    event.preventDefault();
+			    var textAreaElement = $(this).parents('.leave-a-reply').find('#comment');
+			    var checkComment = checkInputTextFieldEmpty(textAreaElement);
+			    if( !checkComment ){
+			      var commentData = {};
+			      commentData.commentText =textAreaElement.val();
+			      commentData.aboveElement = $('.blog-comments');
+			      addCommentDynamically(commentData);
+			    }
+			    else{
+			      alert("enter something")
+			      
+			    }     
+
+			  });
+
+
+			/* ======================================
+			     ON CLICKING OTHER LEVEL PUBLISH COMMENT
+			   ====================================== */
+			   $(document).on('click','div.comment .btn-black',function(){
+
+			    event.preventDefault();
+			    var textAreaElement = $(this).parents('.leave-a-reply-to-comment').find('#comment');
+			    var checkComment = checkInputTextFieldEmpty(textAreaElement);
+			    if( !checkComment ){
+			      var commentData = {};
+			      commentData.commentText = textAreaElement.val();
+			      commentData.aboveElement = $(this).parents('.leave-a-reply-to-comment')
+			                                        .prevAll(".comment-wrap:first"); 
+			      commentData.parentId = commentData.aboveElement.attr("data-commentId");                                        
+			      addCommentDynamically(commentData);
+			    }
+			    else{
+			      alert("nothing written");
+			    }
+
+			  });
+
+
+			/* ======================================
+			     ON CLICKING OTHER REPLIES - ONLY THE INTENT - SO THIS WILL CREATE THE REPLY BOX
+			   ====================================== */
+			   $(document).on('click','div.comment-wrap .reply',function(){
+			    event.preventDefault();
+			    var topCommentDiv = $(this).parents('.comment-wrap');
+			    var commentReply = createCommentReply();
+			    var isCommentBoxOpen = $( ".comment" ).has( ".leave-a-reply-to-comment" ).length;
+			    var isCommentBoxOpenAfterCurrentElement = $( topCommentDiv ).next().hasClass("leave-a-reply-to-comment");
+			    if( isCommentBoxOpen > 0 ){
+			      removeCommentBox();
+			    }
+			    if( isCommentBoxOpen == 0 || isCommentBoxOpenAfterCurrentElement == 0 ){
+			      $( commentReply ).insertAfter(topCommentDiv);
+			      $( commentReply ).find("#comment").focus();
+			    }
+			    $('html, body').animate({
+			          scrollTop: topCommentDiv.offset().top -85
+			        }, 500);
+			  });      
+
+			});
+
+
+		}				
+
+		$.extend( Plugin.prototype, {
+			init: function(options,element) {
+				addBasicHTML(element);
+				getReadyForComments();
+			}	
+
+		} );
+
+		// A really lightweight plugin wrapper around the constructor,
+		// preventing against multiple instantiations
+		$.fn[ pluginName ] = function( options ) {
+			return this.each( function() {
+				if ( !$.data( this, "plugin_" + pluginName ) ) {
+					$.data( this, "plugin_" +
+						pluginName, new Plugin( this, options ) );
+				}
+			} );
+		};
+
+} )( jQuery, window, document );
+
---
+++ js/main.js
@@ -0,0 +1,296 @@
+var imgPath = "../img/user.jpg"; /// Global Variable for user Pic
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////
+/*********** function to create HTML for comment and post body *******************/
+//////////////////////////////////////////////////////////////////////////////////
+function commentAndPostLi(data) {
+    return '<div class="postImg">' +
+        '<img src="' + data.userPic + '">' +
+        '</div>' +
+        '<div class="postContent">' +
+        '<div class="postUsername">' +
+        '<h4>' + data.username + '</h4>' +
+        '</div>' +
+        '<div class="postPara">' +
+        '<p>' + data.postText + '</p>' +
+        '</div>' +
+        '<div class="postTools">' +
+        '<ul class="toolsUl">' +
+        '<li><i class="icon-caret-down icon-3 cursor-pointer vote downvote"></i></li>' +
+        '<li><i class="icon-caret-up icon-3 cursor-pointer vote upvote"></i></li>' +
+        '<li class="voteCounter">0</li>' +
+        '<li class="reply cursor-pointer" id="reply">Reply</li>' +
+        '<li class="share cursor-pointer" id="share">Share</li>' +
+        '</ul>' +
+        '</div>' +
+        '</div>' +
+        '<div class="clearfix"></div>' +
+
+        '<div class="reply-container">' +
+        '<div class="postedReplies">' +
+        '<ul class="commentul"></ul>' +
+        '</div>' +
+        '<div class="replyForm"></div>' +
+        '</div>' +
+        '</div>' +
+        '<div class="clearfix"></div>';
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////////
+/*********** function to show the toastr on post a update *******************/
+//////////////////////////////////////////////////////////////////////////////////
+function showToastr() {
+    var toastr = document.getElementsByClassName('toastr');
+    toastr[0].style.display = 'block';
+
+    setTimeout(function () {
+        toastr[0].style.display = 'none';
+    }, 1000)
+}
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////
+/*********** function to create Post *******************/
+//////////////////////////////////////////////////////////////////////////////////
+function createPost(data, $_this) {
+    if (data.username == "" || data.postText == "") {
+        alert("Please Fill All the Fields")
+    } else {
+        var myLi = document.createElement("li");
+        myLi.className = 'mainPostLi';
+        myLi.innerHTML = commentAndPostLi(data);
+
+        document.getElementById("mainPost").appendChild(myLi);
+
+        var parentFormGroup = getParents($_this, '.formGroup');
+        showToastr();
+        clearFields(parentFormGroup[0].querySelector('.postText'), parentFormGroup[0].querySelector('.uname'));
+    }
+}
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////
+/*********** function to Clear the form Fields After Update or Comment Post *******************/
+//////////////////////////////////////////////////////////////////////////////////
+function clearFields(postTxtAr, postUname) {
+    postTxtAr.value = "";
+    postUname.value = "";
+}
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////
+/*********** function to create Reply Form *******************/
+//////////////////////////////////////////////////////////////////////////////////
+function createReplyBody($_this) {
+
+    var elem = document.createElement("div");
+    elem.className = "mainReplyDiv";
+    elem.innerHTML = '<div class="textarea"><textarea placeholder="Comment" class="postText" id="commentText"></textarea></div>' +
+        '<div class="full">' +
+        '<div class="col-9">' +
+        '<div class="username">' +
+        '<input placeholder="Username" class="uname" id="commentUName">' +
+        '</div>' +
+        '</div>' +
+        '<div class="col-1">' +
+        '<button type="button" class="comBtn cursor-pointer" id="commentBtn">Comment</button>' +
+        '</div>' +
+        '<div class="clearfix"></div>';
+    var ifBodyExist = document.querySelector(".mainReplyDiv");
+
+    // remove reply form if exist
+    if (ifBodyExist) {
+        ifBodyExist.remove();
+    }
+
+    var closestParent = getParents($_this, '.mainPostLi');
+    closestParent[0].appendChild(elem)
+
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////////
+/*********** function to find the Closest parent for targeted element *******************/
+//////////////////////////////////////////////////////////////////////////////////
+var getParents = function (elem, selector) {
+
+    var parents = [];
+    var firstChar;
+    if (selector) {
+        firstChar = selector.charAt(0);
+    }
+    // Get matches
+    for (; elem && elem !== document; elem = elem.parentNode) {
+        if (selector) {
+
+            // If selector is a class
+            if (firstChar === '.') {
+
+                if (elem.classList.contains(selector.substr(1))) {
+                    parents.push(elem);
+                }
+            }
+
+            // If selector is an ID
+            if (firstChar === '#') {
+                if (elem.id === selector.substr(1)) {
+                    parents.push(elem);
+                }
+            }
+
+            // If selector is a data attribute
+            if (firstChar === '[') {
+                if (elem.hasAttribute(selector.substr(1, selector.length - 1))) {
+                    parents.push(elem);
+                }
+            }
+
+            // If selector is a tag
+            if (elem.tagName.toLowerCase() === selector) {
+                parents.push(elem);
+            }
+
+        } else {
+            parents.push(elem);
+        }
+
+    }
+
+    // Return parents if any exist
+    if (parents.length === 0) {
+        return null;
+    } else {
+        return parents;
+    }
+
+};
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////
+/*********** function to Post the Comment/Reply *******************/
+//////////////////////////////////////////////////////////////////////////////////
+function postComment(data, $_this) {
+    var elem = $_this;
+    if (data.postText == "" || data.uname == "") {
+        alert("Fill all the Fields!!")
+    } else {
+        var parents = getParents(elem, '.mainPostLi');
+        var replyBodyUl = document.createElement("li");
+        replyBodyUl.id = "commentContainer";
+        replyBodyUl.className = "mainPostLi";
+        var commentUl = parents[0].querySelector(".commentul");
+        replyBodyUl.innerHTML = commentAndPostLi(data)
+        commentUl.appendChild(replyBodyUl);
+        clearFields(parents[0].querySelector('.postText'), parents[0].querySelector('.uname'));
+        removeCommentFormAfterPost($_this);
+    }
+}
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////
+/*********** remove comment body after successfully posted *******************/
+//////////////////////////////////////////////////////////////////////////////////
+function removeCommentFormAfterPost($_this) {
+    var mainParent = getParents($_this, '.mainReplyDiv');
+    var elemToRemove = mainParent[0];
+    elemToRemove.remove();
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////////
+/*********** function to Down or Up Vote based *******************/
+//////////////////////////////////////////////////////////////////////////////////
+function vote($_this) {
+    var toolbarParent = getParents($_this, '.toolsUl');
+    var upvoteCountContainer = toolbarParent[0].querySelector('.voteCounter');
+    var getCurrentCount = parseInt(upvoteCountContainer.innerHTML);
+
+
+    //Check if clicked button is upvote or down vote and increase and decrease it accordingly
+    if ($_this.classList.contains('upvote')) {
+        var newCount = getCurrentCount + 1;
+    } else {
+        var newCount = getCurrentCount - 1;
+    }
+
+
+    // Change the color of Counter on the basis of Count
+    if (newCount < 0) {
+        upvoteCountContainer.style.color = "red";
+    } else if (newCount === 0) {
+        upvoteCountContainer.style.color = "black";
+    } else {
+        upvoteCountContainer.style.color = "green";
+    }
+
+    upvoteCountContainer.innerHTML = newCount;
+}
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////
+/////////*********** Click Handler for Creating New Post *****************////////
+//////////////////////////////////////////////////////////////////////////////////
+document.getElementById('postBtn').addEventListener('click', function (event) {
+    var $_this = event.target;
+    let myData = {};
+    myData.postText = document.getElementById("postText").value;
+    myData.username = document.getElementById("uname").value;
+    myData.userPic = imgPath;
+    createPost(myData, $_this);
+});
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*********** Click Handler for Dynamically created element and call the function according to clicked button *******************/
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+document.querySelector('body').addEventListener('click', function (event) {
+    var $_this = event.target;
+    // console.log($_this); 
+    if ($_this.classList.contains('reply')) {
+        createReplyBody($_this);
+    } else if ($_this.classList.contains('comBtn')) {
+        var parentForCommentData = getParents($_this, '.mainReplyDiv');
+        var commentData = {};
+        commentData.postText = parentForCommentData[0].querySelector('.postText').value;
+        commentData.username = parentForCommentData[0].querySelector('.uname').value;
+        commentData.userPic = imgPath;
+        postComment(commentData, $_this);
+    } else if ($_this.classList.contains('vote')) {
+        vote($_this)
+    }
+});
